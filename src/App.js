be742519412babde469392dc82ade52217d51a65
Daria Approvato <dariaapprovato@gmail.com>
2022-12-06 13:50:16 -0500

    updated App Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./NavBar";
+import Comments from "./Comments";
+import CommentsForm from "./CommentsForm";
+import Artworks from "./Artworks";
 import './App.css';
 
 function App() {
+  const [artworks, setArtworks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://acnhapi.com/v1/art/")
+      .then((response) => response.json())
+      .then((data) => {
+        setArtworks(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+        <Route exact path="/paintings" element={<Artworks artworks={artworks} type="painting" />} />
+        <Route exact path="/statues" element={<Artworks artworks={artworks} type="statue" />} />
+        <Route exact path="/fossils" element={<Artworks artworks={artworks} type="fossil" />} />
+      </Routes>
+      <Comments />
+      <CommentsForm />
     </div>
   );
 }
