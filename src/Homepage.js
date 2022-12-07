831cf6cd85041f938ea44227a5b352cc7064f3ef
Daria Approvato <dariaapprovato@gmail.com>
2022-12-06 23:02:44 -0500

    updated Homepage

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Homepage() {
+    return (
+        <div>
+            <h1>Welcome to the virtual New Horizons museum!</h1>
+            <h3>Before leaving please feel free to leave a comment for the museum!</h3>
+            <h3>Thank you for visiting! Have a wonderful day!</h3>
+        </div>
+    )
+}
+
+export default Homepage;
