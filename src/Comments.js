3da23569ce11c6960c22ad1d0375a833b5ac1f3a
Daria Approvato <dariaapprovato@gmail.com>
2022-12-08 21:11:16 -0500

    created fetch for comments

--- src/Comments.js
+++ src/Comments.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Comments() {
-    return
+    const [comments, setComments] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3000/comments")
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data)
+            setComments(data);
+          });
+      }, []);
+
+    return (
     <div>
+        {comments.map((comment) =>
+            <h4>{comment.comment} -- {comment.name}</h4>
+        )}
     </div>
+    )
 }
 
 export default Comments;
