97d809d059ccba58aa4754f01577739768dc2695
Daria Approvato <dariaapprovato@gmail.com>
2022-12-09 12:37:37 -0500

    added classnames

--- src/Homepage.js
+++ src/Homepage.js
@@ -26,7 +26,7 @@ function Homepage() {
 
     return (
         <div>
-            <div>
+            <div className="home">
                 <h1>Welcome to the virtual New Horizons museum!</h1>
                 <h3>Before leaving please feel free to leave a comment for the museum!</h3>
                 <h3>Thank you for visiting! Have a wonderful day!</h3>
