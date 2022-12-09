bb66a961d9c302050889b8c98c63d22438c38336
Daria Approvato <dariaapprovato@gmail.com>
2022-12-08 21:52:50 -0500

    updated comments post request

--- src/CommentsForm.js
+++ src/CommentsForm.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 
-function CommentsForm() {
-    const [comment, setComment] = useState([])
+function CommentsForm({ onSubmit }) {
+    const [comment, setComment] = useState({
+        name: "",
+        comment: "",
+    })
 
     function handleSubmit(event) {
         event.preventDefault()
-        // const formData = 
+        onSubmit(comment);
+        event.target.reset()
     }
 
     function handleChange(event) {
-        setComment(event.target.value)
+        setComment({
+            ...comment,
+            [event.target.id]: event.target.value
+        })
     }
 
     console.log(comment)
@@ -17,7 +24,8 @@ function CommentsForm() {
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" onChange={handleChange} />
+                <input type="text" id="name" placeholder="name" onChange={handleChange} />
+                <input type="text" id="comment" placeholder="comment" onChange={handleChange} />
                 <button type="submit">Submit comment</button>
             </form>
         </div>
