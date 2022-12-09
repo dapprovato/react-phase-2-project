614b6a366f54325759ba794ba9d82e3e6262c1b5
Daria Approvato <dariaapprovato@gmail.com>
2022-12-08 21:52:17 -0500

    moved controlled form for comments

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Comments from './Comments';
 import CommentsForm from './CommentsForm';
 
 function Homepage() {
+    const [comments, setComments] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:3000/comments")
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data)
+            setComments(data);
+          });
+      }, []);
+
+    function onSubmit(comment) {
+        setComments([...comments, comment])
+        fetch("http://localhost:3000/comments", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(comment),
+            });
+    }
+
     return (
         <div>
             <div>
@@ -9,7 +31,8 @@ function Homepage() {
                 <h3>Before leaving please feel free to leave a comment for the museum!</h3>
                 <h3>Thank you for visiting! Have a wonderful day!</h3>
             </div>
-            <CommentsForm />
+            <CommentsForm onSubmit={onSubmit} />
+            <Comments comments={comments}/>
         </div>
     )
 }
